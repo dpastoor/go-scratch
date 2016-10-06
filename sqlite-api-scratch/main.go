3561c20a54034b912e9b35c5bb549ad0b4398125
Devin Pastoor <devin.pastoor@gmail.com>
2016-10-06 11:14:52 -0400

    simple sql db

--- sqlite-api-scratch/main.go
+++ sqlite-api-scratch/main.go
@@ -1,38 +1,106 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/kataras/iris"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	os.Remove("./testapi.db")
+	db, err := sql.Open("sqlite3", "./testapi.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	addDataToDB(db)
 	api := iris.Party("/api")
 	v1 := api.Party("/v1")
 	v1.Get("/flu", func(ctx *iris.Context) {
-		ctx.JSON(http.StatusOK, allFromOrg("all"))
+		ctx.JSON(http.StatusOK, allFromOrg(db, "Org1"))
 	})
 	iris.Listen(":8080")
 }
 
 // Person is a simple type for testing sqlite
 type Person struct {
+	ID           int
 	Name         string
 	Organization string
 }
 
-func allFromOrg(org string) []Person {
+func allFromOrg(db *sql.DB, org string) []Person {
 
 	fmt.Println("fetching data for org: ", org)
 	start := time.Now()
-	time.Sleep(1 * time.Second)
-	fmt.Println("got data in: ", time.Since(start))
-	return []Person{
-		Person{"P1", "A"},
-		Person{"P2", "A"},
-		Person{"P1", "B"},
-		Person{"P2", "B"},
+	var People []Person
+	rows, err := db.Query("select ID, Name, Organization from people")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		var org string
+		err = rows.Scan(&id, &name, &org)
+		if err != nil {
+			log.Fatal(err)
+		}
+		People = append(People, Person{id, name, org})
+		fmt.Println(id, name, org)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("got and appended data in: ", time.Since(start))
+	return People
+}
+
+func addDataToDB(db *sql.DB) {
+	start := time.Now()
+	sqlStmt := `
+		create table if not exists people (
+			ID integer not null primary key autoincrement,
+			Name text,
+			Organization text
+			);
+		`
+	_, err := db.Exec(sqlStmt)
+	var names = []string{"James", "John", "Sally", "Sarah"}
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt, err := tx.Prepare("insert into people(Name, Organization) values(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	for i := 0; i < 100; i++ {
+		_, err = stmt.Exec(names[rand.Intn(len(names))], "Org1")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	for i := 0; i < 100; i++ {
+		_, err = stmt.Exec(names[rand.Intn(len(names))], "Org2")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	tx.Commit()
+	fmt.Println("got and appended data in: ", time.Since(start))
 }
