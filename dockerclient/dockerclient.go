b8b326e8cc1444173771b5f295f31aacad622607
Devin Pastoor <devin.pastoor@gmail.com>
2017-04-22 20:16:29 -0400

    docker client messing

---
+++ dockerclient/dockerclient.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	// from https://docs.docker.com/engine/api/get-started/
+	// show all image ids
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, image := range images {
+		fmt.Println(image.ID)
+	}
+}
