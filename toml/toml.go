578b4c631725ca16450136e5d6d283c4bd7d3c7b
Devin Pastoor <devin.pastoor@gmail.com>
2017-04-28 12:26:07 -0400

    test with nested example

--- toml/toml.go
+++ toml/toml.go
@@ -14,7 +14,48 @@ type simpleConfig struct {
 	Perfection []int
 }
 
+type nestedConfig struct {
+	Top    int
+	Nested simpleConfig
+}
+
 func main() {
+	nestedTest()
+}
+
+func nestedTest() {
+	var nested nestedConfig
+	if _, err := toml.DecodeFile("nested.toml", &nested); err != nil {
+		fmt.Printf("could not decode")
+	}
+
+	fmt.Println(nested)
+
+	outputSimple := simpleConfig{
+		Age:        29,
+		Cats:       []string{"Fred", "George"},
+		Pi:         3.1415,
+		Perfection: []int{1, 2, 3},
+	}
+
+	outputNested := nestedConfig{
+		Top:    3,
+		Nested: outputSimple,
+	}
+
+	f, _ := os.Create("output_nested.toml")
+	defer f.Close()
+
+	output := toml.NewEncoder(f)
+	err := output.Encode(outputNested)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("error on encoding: %s", err))
+	}
+	fmt.Println(outputNested)
+
+}
+
+func simpleTest() {
 	var simple simpleConfig
 	if _, err := toml.DecodeFile("simple.toml", &simple); err != nil {
 		fmt.Printf("could not decode")
