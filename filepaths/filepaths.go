96b78b0eb3e8388945d1ce1c4f4e8ab30e421eda
Devin Pastoor <devin.pastoor@gmail.com>
2018-07-21 09:47:06 -0400

    filepath examples

---
+++ filepaths/filepaths.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"crypto/sha512"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+// https://xojoc.pw/justcode/golang-file-tree-traversal.html
+func printFile(ignoreDirs []string) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Print(err)
+			return nil
+		}
+		if info.IsDir() {
+			dir := filepath.Base(path)
+			for _, d := range ignoreDirs {
+				if d == dir {
+					return filepath.SkipDir
+				}
+			}
+		}
+		fmt.Println(path)
+		return nil
+	}
+}
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+	dir := os.Args[1]
+	ignoreDirs := []string{".bzr", ".hg", ".git"}
+	err := filepath.Walk(dir, printFile(ignoreDirs))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+var files = make(map[[sha512.Size]byte]string)
+
+func checkDuplicate(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	if info.IsDir() {
+		return nil
+	}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	digest := sha512.Sum512(data)
+	if v, ok := files[digest]; ok {
+		fmt.Printf("%q is a duplicate of %q\n", path, v)
+	} else {
+		files[digest] = path
+	}
+
+	return nil
+}
+
+// func main() {
+//     log.SetFlags(log.Lshortfile)
+//     dir := os.Args[1]
+//     err := filepath.Walk(dir, checkDuplicate)
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+// }
+
+func du(currentPath string, info os.FileInfo) int64 {
+	size := info.Size()
+	if !info.IsDir() {
+		return size
+	}
+
+	dir, err := os.Open(currentPath)
+	if err != nil {
+		log.Print(err)
+		return size
+	}
+	defer dir.Close()
+
+	fis, err := dir.Readdir(-1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fi := range fis {
+		if fi.Name() == "." || fi.Name() == ".." {
+			continue
+		}
+		size += du(currentPath+"/"+fi.Name(), fi)
+	}
+
+	fmt.Printf("%d %s\n", size, currentPath)
+
+	return size
+}
+
+// func main() {
+//     log.SetFlags(log.Lshortfile)
+//     dir := os.Args[1]
+//     info, err := os.Lstat(dir)
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+//     du(dir, info)
+// }
