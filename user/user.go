f80f536931d6a3b331fbf6f1af09d8cf5f77adba
Devin Pastoor <devin.pastoor@gmail.com>
2018-10-14 14:31:01 -0400

    new UserCacheDir from go 1.11

--- user/user.go
+++ user/user.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/user"
 )
 
@@ -13,4 +14,9 @@ func main() {
 	fmt.Println("HOMEDIR:", usr.HomeDir)
 	fmt.Println("Name:", usr.Name)
 	fmt.Println("Username:", usr.Username)
+	cdir, err := os.UserCacheDir()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("user cache dir: ", cdir)
 }
