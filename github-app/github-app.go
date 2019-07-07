9f581c383cae388b6461e201b9a831c0728cbdb9
Devin Pastoor <devin.pastoor@gmail.com>
2019-07-07 14:28:45 -0400

    use private PEM from env

--- github-app/github-app.go
+++ github-app/github-app.go
@@ -2,21 +2,35 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/google/go-github/github"
-	"github.com/mitchellh/go-homedir"
 )
 
 func main() {
 	// Wrap the shared transport for use with the integration ID 1 authenticating with installation ID 99.
-	fp, _ := homedir.Expand("~/Downloads/kb.2019-07-07.private-key.pem")
-	itr, err := ghinstallation.NewKeyFromFile(http.DefaultTransport, 1, 1, fp)
+	//fp, _ := homedir.Expand("~/Downloads/kb.2019-07-07.private-key.pem")
+	decoded, _ := base64.StdEncoding.DecodeString(os.Getenv("GHE_APP_KB_B64PEM"))
+	fmt.Println("decoded: ", string(decoded))
+	tmpPEM := filepath.Join(os.TempDir(), "temp.PEM")
+	err := ioutil.WriteFile(tmpPEM, decoded, 0644)
+	if err != nil {
+		panic(err)
+	}
+	// defer here in case ghinstall fails
+	defer os.Remove(tmpPEM)
+	itr, err := ghinstallation.NewKeyFromFile(http.DefaultTransport, 1, 1, tmpPEM)
 	if err != nil {
 		// Handle error.
+		panic(err)
 	}
+	os.Remove(tmpPEM)
 	itr.BaseURL = "https://ghe.metrumrg.dev/api/v3"
 	token, err := itr.Token()
 	if err != nil {
